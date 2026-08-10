ab70686e067c702a1de0197efd91868d3eae535e
Joshua Okoronkwo <joshuaokoronkwo54@gmail.com>
2026-08-10 22:47:33 +0100

    Merge listing-published into main

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Public pages
 import Home from "./pages/Home";
-import Signup from "./pages/Signup";
+import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 
 // Dashboard
